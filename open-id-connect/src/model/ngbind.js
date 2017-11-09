58dec4468622198bfa3752868a9c9f6a188c774d
kanishka <kanishkag@wso2.com>
2017-11-09 08:52:57 +0530

    latest commit

--- open-id-connect/src/model/ngbind.js
+++ open-id-connect/src/model/ngbind.js
@@ -1,8 +1,14 @@
 var app = angular.module("login", []); 
 app.controller('loginController', function($scope) {
-    var openid_IMPLICIT ="https://localhost:8243/authorize?client_id=FZHKcK1fZIsNBEvTLnoJF0FAEn0a&redirect_uri=http://localhost:8081/profile&response_type=id_token%20token&scope=openid&nonce=1244434";
-   $scope.submit = function(){
-      window.location.href=openid_IMPLICIT;
+    var api_gateway = "https://localhost:8243/authorize?";
+    var client_id = "client_id=FZHKcK1fZIsNBEvTLnoJF0FAEn0a&";
+    var redirect_uri = "redirect_uri=http://localhost:8081/profile&";
+    var response_type = "response_type=id_token%20token&";
+    var scope = "scope=openid&";
+    var nonce = "nonce=1244434";
+    var openid_implicit=api_gateway+client_id+redirect_uri+response_type+scope+nonce;
+    $scope.submit = function(){
+      window.location.href = openid_implicit;
    }
 });
 
