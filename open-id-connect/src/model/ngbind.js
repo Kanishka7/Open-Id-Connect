b571fb4d3d164b3e7f4fb15d4b705473f71331aa
kanishka <kanishkag@wso2.com>
2017-11-10 10:13:30 +0530

    latest commit

--- open-id-connect/src/model/ngbind.js
+++ open-id-connect/src/model/ngbind.js
@@ -1,12 +1,12 @@
 var app = angular.module("login", []); 
 app.controller('loginController', function($scope) {
     var api_gateway = "https://localhost:8243/authorize?";
-    var client_id = "client_id=FZHKcK1fZIsNBEvTLnoJF0FAEn0a&";
-    var redirect_uri = "redirect_uri=http://localhost:8081/profile&";
-    var response_type = "response_type=id_token%20token&";
-    var scope = "scope=openid&";
-    var nonce = "nonce=1244434";
-    var openid_implicit=api_gateway+client_id+redirect_uri+response_type+scope+nonce;
+    var client_id = "client_id=xQ6u0HAIDiDz4Vnns5Y_UQP4Oesa";
+    var redirect_uri = "redirect_uri=http://localhost:8081/profile";
+    var response_type = "response_type=id_token%20token";
+    var scope = "scope=openid";
+    var nonce = "nonce=1244";
+    var openid_implicit = api_gateway+client_id+"&"+redirect_uri+"&"+response_type+"&"+scope+"&"+nonce;
     $scope.submit = function(){
       window.location.href = openid_implicit;
    }
